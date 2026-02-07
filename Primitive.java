7f32be88b653bd90e01d61de855a5d6e83b173fd
PULLIGANI ROOPESH <roopeshp771@gmail.com>
2026-02-07 10:55:44 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,16 @@
+public class Primitive 
+{
+    public static void main(String[] args)
+    {
+        String str="Roopesh";
+        System.out.println("Student name:"+str);
+        int age=19;
+        System.out.println("Student age:"+age);
+        char grade='A';
+        System.out.println("Student Grade:"+grade);
+        int marks=976;
+        System.out.println("Student marks:"+marks);
+        String str1="Pass";
+        System.out.println("Student status:"+str1);
+    }
+}

618bf1299ee1559cb832c80068cbd4ccea9fa34d
PULLIGANI ROOPESH <roopeshp771@gmail.com>
2026-02-07 10:11:23 +0530

    Add files via upload

---
+++ Literal.java
@@ -0,0 +1,14 @@
+public class Literal
+{
+    public static void main(String[] args)
+    {
+        int a=100;
+        int b=071;
+        int c=0xa12;
+        int d=0b100;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}

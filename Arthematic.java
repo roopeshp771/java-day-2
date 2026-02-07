707e33bb4423e21ca8ede21a3a038dc15c53906e
PULLIGANI ROOPESH <roopeshp771@gmail.com>
2026-02-07 14:51:44 +0530

    Add files via upload

---
+++ Arthematic.java
@@ -0,0 +1,27 @@
+public class Arthematic {
+    public static void main(String[] agrs)
+    {
+        int a=76;
+        int b=23;
+        int c=a+b;
+        System.out.println(c);
+        int a1=-9;
+        if(a1>0)
+        {
+            System.out.println("positive");
+        }
+        else
+        {
+            System.out.println("negative");
+        }
+        int d=8;
+        if(d>0 && d%2==0)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
